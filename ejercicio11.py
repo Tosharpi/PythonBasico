2052737c202e28557cda61f43ec1eaa5bab40723
Tosharpi <160677912+Tosharpi@users.noreply.github.com>
2024-02-26 14:03:01 -0500

    ejercicio 11

---
+++ ejercicio11.py
@@ -0,0 +1,10 @@
+#algoritmo para calcular el area de un circulo
+resp = input('¿desea saber el area del circulo?')
+if resp == 'si' :
+    
+    print('ingrese el radio del circulo: '); radio = float(input())
+
+    area = 3.1416 * (radio * radio)
+    print('el area del circulo es: ', area)
+elif resp != 'si' :
+    print ('¿que?')
