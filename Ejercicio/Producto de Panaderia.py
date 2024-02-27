298d74ef41226270b8f4dcf1acadb3c82645f604
Tosharpi <160677912+Tosharpi@users.noreply.github.com>
2024-02-27 17:29:45 -0500

    Productos panaderias

--- Ejercicio/Producto de Panaderia.py
+++ Ejercicio/Producto de Panaderia.py
@@ -54,6 +54,39 @@ MenuPostre = (
 MenuPostrePrecio = (11500, 12000, 12500, 13000, 13500,
     14000, 14500, 15000, 15500, 16000)
 
+MenuDescuentos = (
+    "Tiramisú: 2 o más unidades por un descuento del 5","%","en el precio final",
+    "Crème brûlée Tiramisú: 2 o más unidades por un descuento del 10","%","en el precio final",
+    "Focaccia Tiramisú: 2 o más unidades por un descuento del 15","%","en el precio final",
+    "Pretzel Tiramisú: 2 o más unidades por un descuento del 20","%","en el precio final",
+    "Eclair Tiramisú: 2 o más unidades por un descuento del 25","%","en el precio final",
+)
+
+MenuDescuentosPorcentaje = (
+    0.05,
+    0.10,
+    0.15,
+    0.20,
+    0.25,
+)
+
+#descuentos
+#pregDescuentos = input('¿Desea saber que descuentos tenemos hoy?')
+#if pregDescuentos == 'si':
+#    for numDesc, nomDesc in enumerate(MenuDescuentos):
+#        print(f""" {numDesc}. {nomDesc} %{MenuDescuentosPorcentaje[numDesc]} de descuento""")
+#    decisionDesc = input('¿deseas llevar algun producto con descuento?')
+#    if decisionDesc == 'si':
+#
+#        codDesc = input('digite el codigo del descuento: ')
+#
+#        print(f"""El descuento seleccionado es: {MenuDescuentos[codDesc]} 
+#        cuyo valor es ${MenuDescuentosPorcentaje[codDesc]}""")
+#
+#
+#         deuda = 
+
+
 print('Bienvenido a panaderia Los Panes')
 print('contamos con tres menús: ')
 print(
@@ -70,8 +103,29 @@ if catMenú == 0:
 
     decision = int(input('¿Que producto desea llevar? digite su codigo: '))
 
+    print('')
+
     print(f"""Seleccionó el producto  {MenuDulce[decision]} 
     cuyo valor es ${MenuDulcePrecio[decision]}""")
+
+    cantidad = int(input('¿cuantas unidades desea llevar?'))
+    deuda = cantidad * MenuDulcePecio[decirsion]
+    print('La cantidad a pagar es: ', deuda)
+
+    pago = int(input('introduzca la cantidad de dinero correspondiente a la deuda: '))
+    control = 0
+
+    while control == 0 :
+    
+        if pago < deuda :
+            vueltos = pago - deuda
+            print('La cantidad de dinero es insuficiente, faltan: ', vueltos)
+            print('ingrese el dinero de nuevo: ')
+
+        if pago > deuda :
+            control = control + 1
+            vueltos = pago - deuda
+            print('Pago realizado, su cambio es: ', vueltos, 'pesos')
 
 #el menu de productos salados
 if catMenú == 2 :
@@ -83,7 +137,50 @@ if catMenú == 2 :
     print(f""" seleccionó el producto  {MenuSalado[decision]} 
     cuyo valor es ${MenuSaladoPrecio[decision]} """)
 
+    cantidad = int(input('¿cuantas unidades desea llevar?'))
+    deuda = cantidad * MenuSaladoPrecio[decision]
+    print('La cantidad a pagar es: ', deuda)
+
+    pago = int(input('introduzca la cantidad de dinero correspondiente a la deuda: '))
+    control = 0
+
+    while control == 0 :
+    
+        if pago < deuda :
+            vueltos = pago - deuda
+            print('La cantidad de dinero es insuficiente, faltan: ', vueltos)
+            print('ingrese el dinero de nuevo: ')
+
+        if pago > deuda :
+            control = control + 1
+            vueltos = pago - deuda
+            print('Pago realizado, su cambio es: ', vueltos, 'pesos')
+
 #el menu de productos tipo postres
 if catMenú == 1 :
     for numProductPostre, nomProductoPostre in enumerate(MenuPostre):
-        print(f""" {numProductPostre}. {MenuPostre[numProductPostre]} ${MenuPostrePrecio[numProductPostre]}""")
+        print(f""" {numProductPostre}. {nomProductoPostre} ${MenuPostrePrecio[numProductPostre]}""")
+
+    decision = int(input('¿Que producto desea llevar? digite su codigo: '))
+
+    print(f""" seleccionó el producto  {MenuPostre[decision]} cuyo valor es ${MenuPostrePrecio[decision]}""")
+    #pago
+
+    cantidad = int(input('¿cuantas unidades desea llevar?'))
+    deuda = cantidad * MenuPostrePrecio[decision]
+    print('La cantidad a pagar es: ', deuda)
+
+    pago = int(input('introduzca la cantidad de dinero correspondiente a la deuda: '))
+    control = 0
+
+    while control == 0 :
+    
+        if pago < deuda :
+            vueltos = pago - deuda
+            print('La cantidad de dinero es insuficiente, faltan: ', vueltos)
+            print('ingrese el dinero de nuevo: ')
+
+        if pago > deuda :
+            control = control + 1
+            vueltos = pago - deuda
+            print('Pago realizado, su cambio es: ', vueltos, 'pesos')
